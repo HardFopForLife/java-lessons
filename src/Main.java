ecb6f6a8a6bfecd326a0eaf4ac6a53fa7a80c16e
HardFopForLife <alp200415@gmail.com>
2026-07-16 00:57:09 +0300

    Доработанная версия программы, с разбором на этапы и жизненный цикл по возрасту

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,51 @@
 import java.util.Scanner;
-import java.time.Year;
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Укажите ваш возраст: ");
-        int age = scanner.nextInt();
-        int curentYear = 2026;
-        int birthYearSimple = curentYear - age;
-        System.out.println("Поздравляю, вы родились в "+birthYearSimple+" году");
-scanner.close();
 
+        //Создаем таким образом сканер, "System.in" говорит: что сканируй внутри
+
+        Scanner scanner = new Scanner(System.in);
+
+        //Делаем красивое приветствие :)
+
+        System.out.println("Приветствую! Давайте знакомится)");
+        System.out.println("Введите ваш возраст (целое число): ");
+
+        //Делаем проверку, если вводит число - "да", символ - "нет"
+
+        if (scanner.hasNextInt()) {
+            int age = scanner.nextInt();
+
+            //Блок проверок условий
+
+            if (age < 0) {
+                System.out.println("Возраст не может быть отрицательным)");
+            } else if (age > 100) {
+                System.out.println("Вы не волшебник!!! " + age + " Столько не живут)");
+            } else {
+                //Проверка жизненного цикла
+                String stage;
+                if (age <= 10) {
+                    System.out.println("Как вы смогли вобще разобраться...");
+                    stage = "Ребенок";
+                } else if (age <= 17) {
+                    System.out.println("Нетрудно наверное было, молодец!");
+                    stage = "Подросток";
+                } else if (age <= 40) {
+                    System.out.println("Разве вас это может интересовать?)");
+                    stage = "Взрослый";
+                } else {
+                    System.out.println("На даче провели интернет?)))");
+                    stage = "Пенсионер";
+                }
+
+                System.out.println("Вам " + age + " лет(-года),\nВаш жизненный этап " + stage);
+            }
+        } else {
+            //Если вместо цифр буквы
+            System.out.println("Ошибка: Нужно ввести целое число, попробуйте снова(");
+        }
+        scanner.close();
+        System.out.println("Программа завершена =)");
     }
 }
